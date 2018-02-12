e745a8b10c6f6f39b20d60dbcfe9a6b99d0fa4cb
“zuzia219 <zuzia219@gmail.com>
2018-02-12 16:04:08 +0100

    second commit

---
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.crud.tasks.repository;
+import com.crud.tasks.domain.Task;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface TaskRepository{
+
+}
---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,6 @@
+package com.crud.tasks.service;
+
+
+public class DbService {
+
+}
