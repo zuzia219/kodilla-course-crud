686f9f60427f725b5164133cda9ac2b424ecec3a
“zuzia219 <zuzia219@gmail.com>
2018-03-04 13:20:56 +0100

    twice

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 import java.util.Optional;
-@NoArgsConstructor
+
 public interface TaskRepository extends CrudRepository<Task, Long> {
     @Override
     List<Task> findAll();
