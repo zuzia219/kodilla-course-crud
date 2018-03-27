02cdbedba3cb80f6d4d34d3e0e34b35744891c23
“zuzia219 <zuzia219@gmail.com>
2018-03-27 13:22:58 +0200

    sender

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -1,12 +1,12 @@
 package com.crud.tasks.config;
 
 import lombok.Getter;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
 public class AdminConfig {
     @Value("${admin.mail}")
-    private String adminMail
+    private String adminMail;
 }
