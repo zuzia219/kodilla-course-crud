6aa17f7e21986f066d66bf22d836f5d50dbaf786
“zuzia219 <zuzia219@gmail.com>
2018-04-23 21:50:22 +0200

    functionality list

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
     @Autowired
@@ -21,6 +24,12 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with trello account");
+        functionality.add("Application allows sending tasks to trello");
+
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "https://zuzia219.github.io/" );
@@ -32,6 +41,8 @@ public class MailCreatorService {
         context.setVariable("company_email" , companyConfig.getCompanyMail());
         context.setVariable("show_button", false);
         context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("created-trello-card-mail", context);
     }
 }
