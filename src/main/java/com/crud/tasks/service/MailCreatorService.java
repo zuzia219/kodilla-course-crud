0f11dc0b6559edf5667b426b209c6658d153f07e
“zuzia219 <zuzia219@gmail.com>
2018-04-23 19:58:43 +0200

    greeting

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -31,6 +31,7 @@ public class MailCreatorService {
         context.setVariable("company_phone", companyConfig.getCompanyPhone());
         context.setVariable("company_email" , companyConfig.getCompanyMail());
         context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
         return templateEngine.process("created-trello-card-mail", context);
     }
 }
